6e5d0933929b38f864d84f44b1c8fa5369f1a6d3
Adel <adel13no@gmail.com>
2023-06-15 18:03:45 +0430

    -swagger spectacular added

--- nouncy/settings.py
+++ nouncy/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "django_filters",
+    'drf_spectacular',
 
     "accounts",
     "announcements",
@@ -130,6 +131,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
--- nouncy/urls.py
+++ nouncy/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView, SpectacularRedocView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/accounts/", include("accounts.urls")),
     path("api/v1/", include("announcements.urls")),
+    path("api/v1/schema/", SpectacularAPIView.as_view(), name='schema'),
+    path("api/v1/swagger-ui/", SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path("api/v1/redoc/", SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
 ]
